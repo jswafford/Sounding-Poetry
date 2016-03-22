ec1433505a7958c5fb253b67cff4a96843aa4537
jswafford <jes8zv@virginia.edu>
2016-03-21 23:31:29 -0400

    Test run on weary blues.

---
+++ wryblues/wearyblues.js
@@ -0,0 +1,608 @@
+data = [
+  {
+    "duration": 0.4689342403632, 
+    "text": "sp", 
+    "end": 0.481405895692, 
+    "amplitude": -0.0, 
+    "start": 0.0124716553288
+  }, 
+  {
+    "duration": 0.6684807256280001, 
+    "text": "DRONING", 
+    "end": 1.14988662132, 
+    "amplitude": -0.000375, 
+    "start": 0.481405895692
+  }, 
+  {
+    "duration": 0.06984126983999994, 
+    "text": "A", 
+    "end": 1.21972789116, 
+    "amplitude": 0.003117, 
+    "start": 1.14988662132
+  }, 
+  {
+    "duration": 0.6485260770899999, 
+    "text": "DROWSY", 
+    "end": 1.86825396825, 
+    "amplitude": 1.1e-05, 
+    "start": 1.21972789116
+  }, 
+  {
+    "duration": 0.7283446712099999, 
+    "text": "SYNCOPATED", 
+    "end": 2.59659863946, 
+    "amplitude": -5.2e-05, 
+    "start": 1.86825396825
+  }, 
+  {
+    "duration": 0.5986394557800003, 
+    "text": "TUNE", 
+    "end": 3.19523809524, 
+    "amplitude": -2e-06, 
+    "start": 2.59659863946
+  }, 
+  {
+    "duration": 0.7283446711999999, 
+    "text": "sp", 
+    "end": 3.92358276644, 
+    "amplitude": -3.5e-05, 
+    "start": 3.19523809524
+  }, 
+  {
+    "duration": 0.3891156462599996, 
+    "text": "ROCKING", 
+    "end": 4.3126984127, 
+    "amplitude": 0.000234, 
+    "start": 3.92358276644
+  }, 
+  {
+    "duration": 0.22947845805, 
+    "text": "BACK", 
+    "end": 4.54217687075, 
+    "amplitude": -0.000227, 
+    "start": 4.3126984127
+  }, 
+  {
+    "duration": 0.12970521542000046, 
+    "text": "AND", 
+    "end": 4.67188208617, 
+    "amplitude": 6.9e-05, 
+    "start": 4.54217687075
+  }, 
+  {
+    "duration": 0.40907029478000023, 
+    "text": "FORTH", 
+    "end": 5.08095238095, 
+    "amplitude": -4.9e-05, 
+    "start": 4.67188208617
+  }, 
+  {
+    "duration": 0.15963718820999961, 
+    "text": "TO", 
+    "end": 5.24058956916, 
+    "amplitude": 0.000172, 
+    "start": 5.08095238095
+  }, 
+  {
+    "duration": 0.04988662131999977, 
+    "text": "A", 
+    "end": 5.29047619048, 
+    "amplitude": -0.002667, 
+    "start": 5.24058956916
+  }, 
+  {
+    "duration": 0.4789115646200006, 
+    "text": "MELLOW", 
+    "end": 5.7693877551, 
+    "amplitude": 0.00024, 
+    "start": 5.29047619048
+  }, 
+  {
+    "duration": 0.5786848072599993, 
+    "text": "CROON", 
+    "end": 6.34807256236, 
+    "amplitude": -7.9e-05, 
+    "start": 5.7693877551
+  }, 
+  {
+    "duration": 0.5587301587300004, 
+    "text": "sp", 
+    "end": 6.90680272109, 
+    "amplitude": 1.6e-05, 
+    "start": 6.34807256236
+  }, 
+  {
+    "duration": 0.12970521541999958, 
+    "text": "I", 
+    "end": 7.03650793651, 
+    "amplitude": -0.000323, 
+    "start": 6.90680272109
+  }, 
+  {
+    "duration": 0.17959183673000023, 
+    "text": "HEARD", 
+    "end": 7.21609977324, 
+    "amplitude": -0.000528, 
+    "start": 7.03650793651
+  }, 
+  {
+    "duration": 0.04988662131999977, 
+    "text": "A", 
+    "end": 7.26598639456, 
+    "amplitude": 0.001854, 
+    "start": 7.21609977324
+  }, 
+  {
+    "duration": 0.49886621315000035, 
+    "text": "NEGRO", 
+    "end": 7.76485260771, 
+    "amplitude": 6.6e-05, 
+    "start": 7.26598639456
+  }, 
+  {
+    "duration": 0.49886621314999946, 
+    "text": "PLAY", 
+    "end": 8.26371882086, 
+    "amplitude": -5.7e-05, 
+    "start": 7.76485260771
+  }, 
+  {
+    "duration": 0.9977324263099998, 
+    "text": "sp", 
+    "end": 9.26145124717, 
+    "amplitude": -1.9e-05, 
+    "start": 8.26371882086
+  }, 
+  {
+    "duration": 0.26938775510000035, 
+    "text": "DOWN", 
+    "end": 9.53083900227, 
+    "amplitude": 0.000302, 
+    "start": 9.26145124717
+  }, 
+  {
+    "duration": 0.1596371882100005, 
+    "text": "ON", 
+    "end": 9.69047619048, 
+    "amplitude": 8.4e-05, 
+    "start": 9.53083900227
+  }, 
+  {
+    "duration": 0.3092970521500007, 
+    "text": "LENOX", 
+    "end": 9.99977324263, 
+    "amplitude": 3.7e-05, 
+    "start": 9.69047619048
+  }, 
+  {
+    "duration": 0.41904761906999966, 
+    "text": "AVENUE", 
+    "end": 10.4188208617, 
+    "amplitude": -4e-06, 
+    "start": 9.99977324263
+  }, 
+  {
+    "duration": 0.1596371881999996, 
+    "text": "THE", 
+    "end": 10.5784580499, 
+    "amplitude": -0.001316, 
+    "start": 10.4188208617
+  }, 
+  {
+    "duration": 0.1696145125000008, 
+    "text": "OTHER", 
+    "end": 10.7480725624, 
+    "amplitude": 0.000729, 
+    "start": 10.5784580499
+  }, 
+  {
+    "duration": 0.3891156461999987, 
+    "text": "NIGHT", 
+    "end": 11.1371882086, 
+    "amplitude": 3e-06, 
+    "start": 10.7480725624
+  }, 
+  {
+    "duration": 0.1696145125000008, 
+    "text": "BY", 
+    "end": 11.3068027211, 
+    "amplitude": -0.000523, 
+    "start": 11.1371882086
+  }, 
+  {
+    "duration": 0.12970521539999957, 
+    "text": "THE", 
+    "end": 11.4365079365, 
+    "amplitude": 0.000585, 
+    "start": 11.3068027211
+  }, 
+  {
+    "duration": 0.4789115646000006, 
+    "text": "PALE", 
+    "end": 11.9154195011, 
+    "amplitude": 9e-06, 
+    "start": 11.4365079365
+  }, 
+  {
+    "duration": 0.41904761909999877, 
+    "text": "DULL", 
+    "end": 12.3344671202, 
+    "amplitude": -3.8e-05, 
+    "start": 11.9154195011
+  }, 
+  {
+    "duration": 0.35918367350000047, 
+    "text": "POWER", 
+    "end": 12.6936507937, 
+    "amplitude": -0.000309, 
+    "start": 12.3344671202
+  }, 
+  {
+    "duration": 0.09977324259999953, 
+    "text": "OF", 
+    "end": 12.7934240363, 
+    "amplitude": 0.001846, 
+    "start": 12.6936507937
+  }, 
+  {
+    "duration": 0.0798185941000007, 
+    "text": "AN", 
+    "end": 12.8732426304, 
+    "amplitude": -0.001791, 
+    "start": 12.7934240363
+  }, 
+  {
+    "duration": 0.41904761900000054, 
+    "text": "OLD", 
+    "end": 13.2922902494, 
+    "amplitude": 0.000249, 
+    "start": 12.8732426304
+  }, 
+  {
+    "duration": 0.39909297059999993, 
+    "text": "GAS", 
+    "end": 13.69138322, 
+    "amplitude": -4.2e-05, 
+    "start": 13.2922902494
+  }, 
+  {
+    "duration": 0.3791383219999993, 
+    "text": "LIGHT", 
+    "end": 14.070521542, 
+    "amplitude": -6.2e-05, 
+    "start": 13.69138322
+  }, 
+  {
+    "duration": 0.5287981858999995, 
+    "text": "sp", 
+    "end": 14.5993197279, 
+    "amplitude": -1.9e-05, 
+    "start": 14.070521542
+  }, 
+  {
+    "duration": 0.10975056690000073, 
+    "text": "HE", 
+    "end": 14.7090702948, 
+    "amplitude": -0.000478, 
+    "start": 14.5993197279
+  }, 
+  {
+    "duration": 0.1596371881999996, 
+    "text": "DID", 
+    "end": 14.868707483, 
+    "amplitude": 0.000532, 
+    "start": 14.7090702948
+  }, 
+  {
+    "duration": 0.039909296999999455, 
+    "text": "A", 
+    "end": 14.90861678, 
+    "amplitude": -0.001049, 
+    "start": 14.868707483
+  }, 
+  {
+    "duration": 0.7083900227000015, 
+    "text": "LAZY", 
+    "end": 15.6170068027, 
+    "amplitude": -0.000104, 
+    "start": 14.90861678
+  }, 
+  {
+    "duration": 0.6285714286000008, 
+    "text": "SWAY", 
+    "end": 16.2455782313, 
+    "amplitude": 0.000137, 
+    "start": 15.6170068027
+  }, 
+  {
+    "duration": 0.5387755101999971, 
+    "text": "sp", 
+    "end": 16.7843537415, 
+    "amplitude": -3.6e-05, 
+    "start": 16.2455782313
+  }, 
+  {
+    "duration": 0.06984126980000127, 
+    "text": "HE", 
+    "end": 16.8541950113, 
+    "amplitude": 0.000463, 
+    "start": 16.7843537415
+  }, 
+  {
+    "duration": 0.1696145125000008, 
+    "text": "DID", 
+    "end": 17.0238095238, 
+    "amplitude": 7.7e-05, 
+    "start": 16.8541950113
+  }, 
+  {
+    "duration": 0.02993197280000004, 
+    "text": "A", 
+    "end": 17.0537414966, 
+    "amplitude": -0.001935, 
+    "start": 17.0238095238
+  }, 
+  {
+    "duration": 0.6185941042999978, 
+    "text": "LAZY", 
+    "end": 17.6723356009, 
+    "amplitude": 5e-05, 
+    "start": 17.0537414966
+  }, 
+  {
+    "duration": 0.638548752800002, 
+    "text": "SWAY", 
+    "end": 18.3108843537, 
+    "amplitude": 7.3e-05, 
+    "start": 17.6723356009
+  }, 
+  {
+    "duration": 0.12970521549999958, 
+    "text": "sp", 
+    "end": 18.4405895692, 
+    "amplitude": -0.000147, 
+    "start": 18.3108843537
+  }, 
+  {
+    "duration": 0.10975056690000073, 
+    "text": "TO", 
+    "end": 18.5503401361, 
+    "amplitude": 0.000176, 
+    "start": 18.4405895692
+  }, 
+  {
+    "duration": 0.08979591829999833, 
+    "text": "THE", 
+    "end": 18.6401360544, 
+    "amplitude": 0.000168, 
+    "start": 18.5503401361
+  }, 
+  {
+    "duration": 0.3891156463000023, 
+    "text": "TUNE", 
+    "end": 19.0292517007, 
+    "amplitude": 8.9e-05, 
+    "start": 18.6401360544
+  }, 
+  {
+    "duration": 0.03990929699999768, 
+    "text": "O", 
+    "end": 19.0691609977, 
+    "amplitude": -0.00025, 
+    "start": 19.0292517007
+  }, 
+  {
+    "duration": 0.27936507940000155, 
+    "text": "THOSE", 
+    "end": 19.3485260771, 
+    "amplitude": -0.000298, 
+    "start": 19.0691609977
+  }, 
+  {
+    "duration": 0.4589569161, 
+    "text": "WEARY", 
+    "end": 19.8074829932, 
+    "amplitude": -0.000165, 
+    "start": 19.3485260771
+  }, 
+  {
+    "duration": 0.6086167799999984, 
+    "text": "BLUES", 
+    "end": 20.4160997732, 
+    "amplitude": 9.9e-05, 
+    "start": 19.8074829932
+  }, 
+  {
+    "duration": 1.5065759637999996, 
+    "text": "sp", 
+    "end": 21.922675737, 
+    "amplitude": -1.2e-05, 
+    "start": 20.4160997732
+  }, 
+  {
+    "duration": 0.19954648520000262, 
+    "text": "WITH", 
+    "end": 22.1222222222, 
+    "amplitude": -5.2e-05, 
+    "start": 21.922675737
+  }, 
+  {
+    "duration": 0.1297052153999978, 
+    "text": "HIS", 
+    "end": 22.2519274376, 
+    "amplitude": 0.000365, 
+    "start": 22.1222222222
+  }, 
+  {
+    "duration": 0.3292517007000022, 
+    "text": "EBONY", 
+    "end": 22.5811791383, 
+    "amplitude": -0.000377, 
+    "start": 22.2519274376
+  }, 
+  {
+    "duration": 0.3990929704999999, 
+    "text": "HANDS", 
+    "end": 22.9802721088, 
+    "amplitude": 5.4e-05, 
+    "start": 22.5811791383
+  }, 
+  {
+    "duration": 0.10975056689999718, 
+    "text": "ON", 
+    "end": 23.0900226757, 
+    "amplitude": -0.000599, 
+    "start": 22.9802721088
+  }, 
+  {
+    "duration": 0.19954648530000085, 
+    "text": "EACH", 
+    "end": 23.289569161, 
+    "amplitude": 0.000366, 
+    "start": 23.0900226757
+  }, 
+  {
+    "duration": 0.42902494329999996, 
+    "text": "IVORY", 
+    "end": 23.7185941043, 
+    "amplitude": 0.000132, 
+    "start": 23.289569161
+  }, 
+  {
+    "duration": 0.35918367350000224, 
+    "text": "KEY", 
+    "end": 24.0777777778, 
+    "amplitude": 5.6e-05, 
+    "start": 23.7185941043
+  }, 
+  {
+    "duration": 0.11972789109999837, 
+    "text": "HE", 
+    "end": 24.1975056689, 
+    "amplitude": 0.000108, 
+    "start": 24.0777777778
+  }, 
+  {
+    "duration": 0.2294784581000009, 
+    "text": "MADE", 
+    "end": 24.426984127, 
+    "amplitude": 2.1e-05, 
+    "start": 24.1975056689
+  }, 
+  {
+    "duration": 0.18956916099999788, 
+    "text": "THAT", 
+    "end": 24.616553288, 
+    "amplitude": -0.000134, 
+    "start": 24.426984127
+  }, 
+  {
+    "duration": 0.33922902489999984, 
+    "text": "POOR", 
+    "end": 24.9557823129, 
+    "amplitude": -0.000124, 
+    "start": 24.616553288
+  }, 
+  {
+    "duration": 0.37913832200000286, 
+    "text": "PIANO", 
+    "end": 25.3349206349, 
+    "amplitude": 0.000169, 
+    "start": 24.9557823129
+  }, 
+  {
+    "duration": 0.7183673469999974, 
+    "text": "MOAN", 
+    "end": 26.0532879819, 
+    "amplitude": -0.000221, 
+    "start": 25.3349206349
+  }, 
+  {
+    "duration": 0.13968253960000254, 
+    "text": "WITH", 
+    "end": 26.1929705215, 
+    "amplitude": 0.000854, 
+    "start": 26.0532879819
+  }, 
+  {
+    "duration": 0.6285714285999973, 
+    "text": "MELODY", 
+    "end": 26.8215419501, 
+    "amplitude": -2.7e-05, 
+    "start": 26.1929705215
+  }, 
+  {
+    "duration": 0.498866213200003, 
+    "text": "sp", 
+    "end": 27.3204081633, 
+    "amplitude": -3e-06, 
+    "start": 26.8215419501
+  }, 
+  {
+    "duration": 0.3791383219999993, 
+    "text": "O", 
+    "end": 27.6995464853, 
+    "amplitude": 8.7e-05, 
+    "start": 27.3204081633
+  }, 
+  {
+    "duration": 0.628571428499999, 
+    "text": "BLUES", 
+    "end": 28.3281179138, 
+    "amplitude": -2.7e-05, 
+    "start": 27.6995464853
+  }, 
+  {
+    "duration": 1.7061224489999987, 
+    "text": "sp", 
+    "end": 30.0342403628, 
+    "amplitude": -1.7e-05, 
+    "start": 28.3281179138
+  }
+];
+// Filter out the spaces
+data = _.filter(data, function(x){return x.text != "sp";});
+window.onload = function() {
+  words = d3.selectAll("span.word");
+  window.words = words;
+  words.data(data);
+  function apply(mode) {
+    d3.select("div.text-wrapper").classed({
+      "mode-area": false,
+      "mode-width": false,
+      "mode-scale": false,
+    }).classed("mode-"+mode, true);
+    words.style("width", null);
+    words.style("font-size", null);
+    if(mode === "area") {
+      // Area mode - each word fits in a box whose area is proportional to the word's duration.
+      // Pros: shortish words with long durations look larger than long words with similar duration.
+      // Cons: short words with long durations look larger than shortish words with medium duration.
+      // For example, if "I" and "heard" are each about the same length, "I" will be MUCH bigger than "heard"
+      words.style("font-size", function(d) {
+        var box = this.getBoundingClientRect();
+        // Compute a scaling factor that will convert curr_area to target_area
+        var curr_area = box.width*box.height;
+        var target_area = d.duration*10000;
+        var scale = Math.pow(target_area/curr_area, .5);
+        return scale*100+"%";
+      });
+    } else if(mode === "width") {
+      // Width mode-- each word fits in a box whose width is proportional to the word's duration.
+      // Pros: Easy to read, less misleading
+      // Cons: Ugly.
+      words.style("width", function(d) {
+        return (d.duration/.55)*150+"px";
+      });
+    } else if(mode === "scale") {
+      // Scale mode--each word's font size is proportional to its duration.
+      // Pros: Easy to read and find patterns
+      // Cons: short words with long duration look smaller than long words with shorter or equal duration (e.g. "syncopated" and "Moan")
+      words.style("font-size", function(d) {
+        return (d.duration/.55)*30+10+"px";
+      });
+    }
+  }
+  window.apply = apply;
+  apply("area");
+}
