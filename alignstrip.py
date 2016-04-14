ccc131daffd08f7d22040a9cdf0d07005465abf1
Brandon Walsh <walshb@wlu.edu>
2016-04-14 15:18:29 -0400

    refactoring

--- alignstrip.py
+++ alignstrip.py
@@ -7,32 +7,40 @@ import subprocess as sp
 
 import audiolabel
 
+TEXTGRID = "NEED_DEFAULT_VALUE_HERE"
+WAV = "NEED_DEFAULT_VALUE_HERE"
+OUTDIR = "NEED_DEFAULT_VALUE_HERE"
+
 # TODO: document arg parsing here.
 
 # adds arguments for parsing
 
+
 def parse_args(argv=None):
-    """Parses the command line. If no arguments are given, it will default to a given set of commands."""
+    """Parses the command line.
+    If no arguments are given, it will default to a given set of commands."""
 
     parser = argparse.ArgumentParser(description=__doc__)
-
     parser.add_argument('-t', '--textgrid', dest='textgrid', action='store',
-                        default=NEED_DEFAULT_VALUE_HERE,
-                        help='Describes what the argument does. Default = {}.'.format(NEED_DEFAULT_VALUE_HERE))
+                        default=TEXTGRID,
+                        help='Describes what the argument does. Default = {}.'.format(TEXTGRID))
     parser.add_argument('-w', '--wav', dest='wav',
-                        default=NEED_DEFAULT_VALUE_HERE,
-                        help='Describes what the argument does = {}'.format(NEED_DEFAULT_VALUE_HERE))
+                        default=WAV,
+                        help='Describes what the argument does = {}'.format(WAV))
     parser.add_argument('-o', '--outdir', dest='outdir',
-                        action='store', default=NEED_DEFAULT_VALUE_HERE,
+                        action='store', default=OUTDIR,
                         help='Describes what the argument does')
     return parser.parse_args(argv)
+
 
 def main():
     """The main function that is run when not imported in another module."""
 
     # parses arguments
 
-    # when the thing runs using our functions, delete following lines and uncomment the last line to use our own parsing function.
+    # when the thing runs using our functions,
+    # delete following lines and uncomment
+    # the last line to use our own parsing function.
     parser = argparse.ArgumentParser()
     parser.add_argument('textgrid')
     parser.add_argument('wav')
@@ -55,11 +63,11 @@ def main():
         lines = amplitude.stderr.read().splitlines()
 
         result.append(dict(
-            start = label.t1,
-            end = label.t2,
-            text = label.text,
-            duration = label.t2-label.t1,
-            amplitude = float(lines[7].split()[2])
+            start=label.t1,
+            end=label.t2,
+            text=label.text,
+            duration=label.t2-label.t1,
+            amplitude=float(lines[7].split()[2])
         ))
     raw_text = json.dumps(result, indent=2)
     open('{}/data.json'.format(args.outdir), 'w').write(raw_text)
