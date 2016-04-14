bad1681ee346e6b9eb1cf2e663824489c37555f8
Brandon Walsh <walshb@wlu.edu>
2016-04-14 14:59:24 -0400

    stubs out arg parsing. starts to modularize things.

--- alignstrip.py
+++ alignstrip.py
@@ -11,35 +11,55 @@ import audiolabel
 
 # adds arguments for parsing
 
-parser = argparse.ArgumentParser()
-parser.add_argument('textgrid')
-parser.add_argument('wav')
-parser.add_argument('outdir')
+def parse_args(argv=None):
+    """Parses the command line. If no arguments are given, it will default to a given set of commands."""
 
-# parses arguments
+    parser = argparse.ArgumentParser(description=__doc__)
 
-args = parser.parse_args()
+    parser.add_argument('-t', '--textgrid', dest='textgrid', action='store',
+                        default=NEED_DEFAULT_VALUE_HERE,
+                        help='Describes what the argument does. Default = {}.'.format(NEED_DEFAULT_VALUE_HERE))
+    parser.add_argument('-w', '--wav', dest='wav',
+                        default=NEED_DEFAULT_VALUE_HERE,
+                        help='Describes what the argument does = {}'.format(NEED_DEFAULT_VALUE_HERE))
+    parser.add_argument('-o', '--outdir', dest='outdir',
+                        action='store', default=NEED_DEFAULT_VALUE_HERE,
+                        help='Describes what the argument does')
+    return parser.parse_args(argv)
 
-data = audiolabel.LabelManager(from_file=args.textgrid, from_type="praat")
-tier = data.tier('word')
-result = []
+def main():
+    """The main function that is run when not imported in another module."""
 
-for i, label in enumerate(tier):
-    # to make excerpts split by word
-    filename = '{}/excerpts/excerpt{}.wav'.format(args.outdir, i)
-    trimcommand = shlex.split('sox {} {} trim {} ={}'.format(args.wav, filename, label.t1, label.t2))
-    ampcommand = shlex.split('sox {} -n stat'.format(filename))
-    trim = sp.Popen(trimcommand, stderr=sp.PIPE)
-    trim.wait()
-    amplitude = sp.Popen(ampcommand, stderr=sp.PIPE)
-    lines = amplitude.stderr.read().splitlines()
+    # parses arguments
 
-    result.append(dict(
-        start = label.t1,
-        end = label.t2,
-        text = label.text,
-        duration = label.t2-label.t1,
-        amplitude = float(lines[7].split()[2])
-    ))
-raw_text = json.dumps(result, indent=2)
-open('{}/data.json'.format(args.outdir), 'w').write(raw_text)
+    # when the thing runs using our functions, delete following lines and uncomment the last line to use our own parsing function.
+    parser = argparse.ArgumentParser()
+    parser.add_argument('textgrid')
+    parser.add_argument('wav')
+    parser.add_argument('outdir')
+    args = parser.parse_args()
+    # args = parse_args()
+
+    data = audiolabel.LabelManager(from_file=args.textgrid, from_type="praat")
+    tier = data.tier('word')
+    result = []
+
+    for i, label in enumerate(tier):
+        # to make excerpts split by word
+        filename = '{}/excerpts/excerpt{}.wav'.format(args.outdir, i)
+        trimcommand = shlex.split('sox {} {} trim {} ={}'.format(args.wav, filename, label.t1, label.t2))
+        ampcommand = shlex.split('sox {} -n stat'.format(filename))
+        trim = sp.Popen(trimcommand, stderr=sp.PIPE)
+        trim.wait()
+        amplitude = sp.Popen(ampcommand, stderr=sp.PIPE)
+        lines = amplitude.stderr.read().splitlines()
+
+        result.append(dict(
+            start = label.t1,
+            end = label.t2,
+            text = label.text,
+            duration = label.t2-label.t1,
+            amplitude = float(lines[7].split()[2])
+        ))
+    raw_text = json.dumps(result, indent=2)
+    open('{}/data.json'.format(args.outdir), 'w').write(raw_text)
